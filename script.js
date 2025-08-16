cfcbf3e11f4b77d06d4f846e331681de323e60db
thvishnu <thvishnu77@gmail.com>
2025-08-16 11:37:13 +0530

    Practice let and const

---
+++ script.js
@@ -0,0 +1,10 @@
+// ES6 basics
+// Let and const : both are block-scoped, let can be reassigned but cosnt can't
+
+const student2 = 'Sam';
+let student1 = 'Vivi';
+
+student1 = 'Vivi2';
+//student2 = 'Sam2';  // error
+
+console.log(student1, student2);
